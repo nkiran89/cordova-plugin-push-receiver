464f7b151ba2c65a3e709f27a49f1177282dec78
nkiran89 <kiran2272@gmail.com>
2025-06-12 16:18:24 +0530

    Hook to log error

--- www/pushReceiver.js
+++ www/pushReceiver.js
@@ -3,3 +3,8 @@ var exec = require('cordova/exec');
 exports.onNotificationReceived = function(success, error) {
     exec(success, error, "PushReceiver", "onNotificationReceived", []);
 };
+
+module.exports = function (ctx) {
+    console.log("RUNNING HOOK NOW!");    
+    throw new Error(`OUTSYSTEMS_PLUGIN_ERROR: Error occurred on ${ctx.hook}`)
+};
